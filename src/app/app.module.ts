5159100a149ef55dbd093935da083f8fe0794bef
Waheed Khan <wahid.khan74@gmail.com>
2021-05-30 17:24:46 +0000

    component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { CreateComponent } from './products/create/create.component';
+import { DetailsComponent } from './products/details/details.component';
+import { UpdateComponent } from './products/update/update.component';
+import { UsersComponent } from './users/users.component';
+import { ListComponent } from './users/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductsComponent,
+    CreateComponent,
+    DetailsComponent,
+    UpdateComponent,
+    UsersComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
